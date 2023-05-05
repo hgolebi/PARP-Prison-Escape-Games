da3dd40fb664962f462056998a8b7eb435f76f42
hgolebi1 <01161897@pw.edu.pl>
2023-05-05 23:03:52 +0200

    invastigating objects

--- prison_escape.pl
+++ prison_escape.pl
@@ -18,14 +18,38 @@ borders(guard_room, gym).
 borders(kitchen, guard_room).
 borders(shower_room, guard_room).
 borders(gym, guard_room).   
+/* MAPA DO DOKONCZENIA */
+
+
+/* Objects in rooms definition */
+there_is(your_bed, cell2).
+there_is(old_mans_bed, cell2).
+there_is(toilet, cell2).
+there_is(desk, guard_room).
+/* WYKONCZENIE POKOI DO DOKONCZENIA */
+
+/* Items locations definition */
+there_is(poop, toilet).
+there_is(coin, toilet).
 
 at(cell2).
 
+/* These rule(s) tell what is going on around you*/
+
 look :-
     at(Place),
-    nl, write("You're currently at "), write(Place), nl, nl,
-    write("You can go to:"), nl,
+    nl, write("You're currently at "), write(Place), nl,
+    nl, write("You can see: "), nl,
+    list_objects(Place),
+    nl, write("You can go to:"), nl,
     available_destinations(Place), nl.
+
+list_objects(Place) :-
+    there_is(Object, Place),
+    write("* "), write(Object), nl,
+    fail.
+
+list_objects(_).
 
 available_destinations(Place) :-
     borders(Place, Destination),
@@ -33,6 +57,8 @@ available_destinations(Place) :-
     fail.
 
 available_destinations(_).
+
+/* These rule(s) tell(s) how to move to different locations*/
 
 go(Destination) :-
     at(Place),
@@ -44,6 +70,54 @@ go(Destination) :-
 go(_) :-
     write("You can,t go there!").
 
+/* These rule(s) tell what you can find in object*/
+
+investigate(Object) :-
+    at(Place),
+    there_is(Object, Place),
+    empty(Object),
+    nl, write(Object), write(" is empty."), nl,
+    !, nl.
+
+investigate(Object) :-
+    at(Place),
+    there_is(Object, Place),
+    nl, write("In "), write(Object), write(" you can find:"), nl,
+    fail.
+
+investigate(Object) :-
+    at(Place),
+    there_is(Object, Place),
+    there_is(Item, Object),
+    write("* "), write(Item), nl,
+    fail.
+
+investigate(Object) :-
+    at(Place),
+    there_is(Object, Place),
+    !, nl.
+
+investigate(Object) :-
+    nl, write("Cannot investigate "), write(Object), nl.
+
+there_is(hammer, bed).
+
+empty(Object) :- 
+    \+ there_is(_, Object).
+
+
+commands :-
+    nl,
+        write('Enter commands using standard Prolog syntax.'), nl,
+        write('Available commands are:'), nl,
+        write('start.             -- to start the game.'), nl,
+        write('go(Destination)    -- to go to selected destination.'), nl,
+        write('look.              -- to look around you again.'), nl,
+        write('commands.          -- to see this message again.'), nl,
+        write('halt.              -- to end the game and quit.'), nl,
+        nl.
+
 start :-
-    nl, write("Welcome to Andrew Tate Prison Escape"), nl, nl,
+    nl, write("Welcome to Andrew Tate Prison Escape"), nl,
+    commands,
     look.
