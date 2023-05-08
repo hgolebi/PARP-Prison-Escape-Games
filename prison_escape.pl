554f9297b2a539a8b077ce0f78a3f39e199adf0e
hgolebi1 <01161897@pw.edu.pl>
2023-05-08 14:30:03 +0200

    showering prisoner dialogue, some fixes in other dialogues

--- prison_escape.pl
+++ prison_escape.pl
@@ -108,6 +108,7 @@ there_is(playboy_magazine, shelf).
 there_is(flashligth, desk).
 there_is(cell1_key, guard).
 there_is(towel, bench).
+there_is(batteries, pillow).
 
 /* Starting in cell2 */
 at(cell2).
@@ -173,6 +174,10 @@ go(Destination) :-
 go(_) :-
     write("You can't go there!").
 
+debuggo(Place) :-
+    retract(at(_)),
+    assert(at(Place)).
+
 /* This rules tell how to unclock room */
 unlock(Room) :-
     at(Place),
@@ -220,17 +225,23 @@ unlock(ventilation) :-
 unlock(Room) :-
     nl, write("Cannot unlock "), write(Room), nl.
 
-/* For debuggin purpous only */
+/* For debugging purpous only */
 unlockall :-
     retractall(locked(_)).
 
 /* These rule(s) tell what you can find in object*/
 
+investigate(old_man) :-
+    at(cell2),
+    nl, write("Old Man: You filthy rat, keep your hands close!!"), nl,
+    !, nl.
+
+
 investigate(Object) :-
     at(Place),
     there_is(Object, Place),
     empty(Object),
-    nl, write(Object), write(" is empty."), nl,
+    nl, write("Nothing to be found here."), nl,
     !, nl.
 
 investigate(Object) :-
@@ -258,6 +269,23 @@ empty(Object) :-
     \+ there_is(_, Object).
 
 /* These rules describe how to pick up an object. */
+
+take(towel) :-
+    at(gym),
+    there_is(towel, bench),
+    there_is(gym_guy, gym),
+    nl, write("Gym Guy: Hey what do you think you're doing?! That's my towel!"), nl,
+    write("You: Can I borrow it?"), nl,
+    write("Gym Guy: Forget it. I need it for my workout."), nl,
+    !, nl.
+
+take(towel) :-
+    at(gym),
+    there_is(towel, bench),
+    \+ there_is(gym_guy, gym),
+    retract(there_is(towel, bench)),
+    assert(holding(towel)),
+    !, nl.
 
 take(Item) :-
     holding(Item),
@@ -341,38 +369,67 @@ dialogue(old_man) :-
     quest_done(quest2, old_man), nl,
     write("You: What about the light?"), nl,
     write("Old Man: You can break the ventilation hole in the hallway and get into the room with fuses, where you turn off the light."), nl,
-    write("You: Holy Chicken Trolley,  that's my opportunity!!"), nl.
+    write("You: Holy Chicken Trolley,  that's my opportunity!!"), nl,
+    assert(borders(hallway, ventilation)).
 
 dialogue(gym_guy) :-
     (\+ quest_done(meal_quest, gym_guy)),
-    write("You see a strong guy that exhausted after his training."), nl,
+    write("You see a strong guy that is exhausted after his training."), nl,
     write("You: Hey! I have a case. Could I do something for you in return for a small favor?"), nl,
     write("Gym Guy: You little man, what would you need help for?"), nl,
     write("You: To break the ventilation hole."), nl,
-    write("Gym Guy: It's a piece of cake for me. Bring me great meal casue I need to refill my carbs. Then I'll do the job."), nl.
+    write("Gym Guy: It's a piece of cake for me. Bring me great meal cause I need to refill my carbs. Then I'll do the job."), nl.
 
 dialogue(gym_guy) :-
     quest_done(meal_quest, gym_guy),
     write("You: So, will you help me with yout muscles?"), nl,
     write("Gym Guy: Yeah, the meal was great. Take me to the place."), nl,
-    write("You and the Gym Guy went to the ventilation hole and broke it. Now you're back at the gym."), nl,
-    write("You: There is just one more thing... Could I borrow your towel? Just for some time."), nl,
-    write("Gym Guy: You lucky bastard, be thankful that I am happy after the workout... Here you have it, up to 2h it must be returned."), nl,
-    write("Gym Guy handed you a towel."), nl,
-    assert(holding(towel)).
+    write("You and the Gym Guy went to the ventilation grid and broke it."), nl,
+    retract(at(gym)),
+    assert(at(hallway)),
+    retract(there_is(gym_guy, gym)),
+    assert(there_is(gym_guy, hallway)),
+    retract(there_is(ventilation_grid, hallway)),
+    retract(locked(ventilation)).
+
+dialogue(showering_prisoner) :-
+    \+ quest_done(towel_quest, showering_prisoner),
+    nl, write("Prisoner: Hey what are you looking at?!"), nl,
+    write("You: I was just.."), nl,    
+    write("Prisoner: Get out now!! Or actually, wait.. Bring me the towel!"), nl,
+    write("You: Why would I?"), nl,
+    write("Prisoner: You dare to ask?! Fine, if you decide to help me I'll give you something in return."), nl,   
+    !, nl. 
+
+dialogue(showering_prisoner) :-
+    quest_done(towel_quest, showering_prisoner),
+    nl, write("You: You received your towel. What about my reward?"), nl,
+    write("Prisoner: Hmm... I don't have anything on me, but.."), nl,
+    write("You: But what?!"), nl,
+    write("Prisoners: Do you want some batteries?"), nl,
+    write("You: Why would I need batteries?"), nl,
+    write("Prisoner: I dunno, maybe to power up a flashlight or something.."), nl,
+    write("You: Fine, give me those batteries!"), nl,
+    write("Prisoner: You can find them under a pillow in cell 3."), nl,
+    write("You: Oh, you don't have them on you, right.."), nl,
+    assert(there_is(pillow, cell3)),
+    !, nl.
+
 
 dialogue(chef) :-
     (\+ quest_done(coffee_quest, chef)),
     write("You: Hi! I've heard that you're the best chef in here. Could you make me a signature meal?"), nl,
     write("Chef: Nice words won't be enough. I'am actually pretty tired, if you could bring me some coffee then I'll cook something."), nl,
-    write("You: Not a problem, I'll be in a moment."), nl.
+    write("You: I should have some in my cell, I'll be in a moment."), nl,
+    assert(there_is(coffee, table)).
 
 dialogue(chef) :-
     quest_done(coffee_quest, chef),
     write("You: Now you're quite caffenaited, aren't you?"), nl,
     write("Chef: Yeah, thanks. I'll cook something quickly."), nl,
     write("After few minutes chef hands you a hot meal."), nl,
-    write("You: Thanks a lot, bye."), nl.
+    write("You: Thanks a lot, bye."), nl,
+    assert(holding(great_meal)).
 
 
 /* These rules describe how to give an item to a person */
@@ -392,7 +449,8 @@ give(Item, old_man) :-
     ;   assert(quest_done(quest2, old_man))
     ),
     write("Old Man: Ah, you've brought the cigarettes. Good."), nl,
-    write("You hand the cigarettes to the Old Man."), nl.
+    write("You hand the cigarettes to the Old Man."), nl,
+    !, nl.
 
 give(Item, old_man) :-
     at(Place),
@@ -401,14 +459,16 @@ give(Item, old_man) :-
     Item = cigarettes,
     cigarettes(Count),
     Count < 5,
-    write("Old Man: You don't have enough cigarettes."), nl.
+    write("Old Man: You don't have enough cigarettes."), nl,
+    !, nl.
 
 give(Item, old_man) :-
     at(Place),
     there_is(old_man, Place),
     holding(Item),
     Item \= cigarettes,
-    write("Old Man: I don't want that item."), nl.
+    write("Old Man: I don't want that item."), nl,
+    !, nl.
 
 give(Item, gym_guy) :-
     at(Place),
@@ -418,20 +478,23 @@ give(Item, gym_guy) :-
     retract(holding(Item)),
     assert(quest_done(meal_quest, gym_guy)),
     write("Gym Guy: Just on time, give me that."), nl,
-    write("You hand the meal to the Gym Guy."), nl.
+    write("You hand the meal to the Gym Guy."), nl,
+    !, nl.
 
 give(Item, gym_guy) :-
     at(Place),
     there_is(gym_guy, Place),
     holding(Item),
     Item \= great_meal,
-    write("Gym Guy: I don't want that item."), nl.
+    write("Gym Guy: I don't want that item."), nl,
+    !, nl.
 
 give(Item, gym_guy) :-
     at(Place),
     there_is(gym_guy, Place),
     \+ holding(Item),
-    write("Gym Guy: You don't have the meal."), nl.
+    write("Gym Guy: You don't have the meal."), nl,
+    !, nl.
 
 give(Item, chef) :-
     at(Place),
@@ -441,20 +504,23 @@ give(Item, chef) :-
     retract(holding(Item)),
     assert(quest_done(coffee_quest, chef)),
     write("Gym Guy: Oh, you have the coffee. I need a boost of energy."), nl,
-    write("You hand the cofffee to the Chef."), nl.
+    write("You hand the cofffee to the Chef."), nl,
+    !, nl.
 
 give(Item, chef) :-
     at(Place),
     there_is(chef, Place),
     holding(Item),
     Item \= coffee,
-    write("Chef: I don't want that item."), nl.
+    write("Chef: I don't want that item."), nl,
+    !, nl.
 
 give(Item, gym_guy) :-
     at(Place),
     there_is(chef, Place),
     \+ holding(Item),
-    write("Chef: You don't have the coffee."), nl.
+    write("Chef: You don't have the coffee."), nl,
+    !, nl.
 
 give(_, _) :-
     nl, write("This person doesn't want that item."), nl.
